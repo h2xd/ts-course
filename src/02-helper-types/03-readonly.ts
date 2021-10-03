7378d20c260a4a24eb4d11600b13c76ad714e8f9
Andreas Schujkow <andreas.schujkow@sumcumo.com>
2021-10-03 21:02:33 +0200

    fix: yo what happend?! The past send me a message

--- src/02-helper-types/03-readonly.ts
+++ src/02-helper-types/03-readonly.ts
@@ -2,17 +2,26 @@ type Debts = {
   innkeeper: number
   city: number
   cobbler: number
+  others: number[]
 }
 
 const AdventurerDebts: Debts = {
   innkeeper: 1_500,
   city: 3_000_000,
   cobbler: 500, // no shoes, no life
+  others: [422]
 }
 
 
 function writeInsolvencyLetter(): Readonly<Debts> {
   // ... Hopefully Peter Zwegart won't notice that action
+  return Object.freeze(AdventurerDebts)
 }
 
 const AdventurerInsolvency = writeInsolvencyLetter()
+
+/**
+ * It's me Andrej from the past!
+ * I just realized that you should explain `ReadonlyArray`
+ * after you finished with 07-Omit
+ */
